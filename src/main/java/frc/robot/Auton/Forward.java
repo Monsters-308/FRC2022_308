ee5bed1b1685226e72d95126f8b83863f064ac88
Austin Spencer <aspencer527@gmail.com>
2022-02-10 18:34:06 -0500

    first commit

---
+++ src/main/java/frc/robot/Auton/Forward.java
@@ -0,0 +1,39 @@
+package frc.robot.Auton;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+public class Forward {
+    private double speed;
+    private double time;
+    private Timer timer = new Timer();
+
+    /**
+     * Initialize a "Forward" auton class.
+     * 
+     * @param speed controls the speed and direction
+     * @param time  controls the amount of time that the auton can run after its
+     *              creation
+     */
+    public Forward(double speed, double time) {
+        this.speed = speed;
+        this.time = time;
+        timer.reset();
+        timer.start();
+    }
+
+    /**
+     * will loop the auton once
+     * 
+     * @return will return a list containing the speed for the left and right
+     *         motors.
+     */
+    public void loop(DifferentialDrive motors) {
+        if (timer.get() < time) {
+            motors.tankDrive(speed, speed);
+        }
+        else{
+            motors.tankDrive(0.0,0.0);
+        }
+    }
+}
---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,81 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+// Author : FIRST Robotics team Monsters 308
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import frc.robot.Auton.Forward;
+
+/**
+
+ * if necessary, view the TimedRobot documentation. If you change the
+ * name of this class or
+ * the package after creating this project, you must also update the manifest
+ * file in the resource
+ * directory.
+ */
+public class Robot extends TimedRobot {
+    private final PWMSparkMax leftDrive1 = new PWMSparkMax(0);
+    private final PWMSparkMax leftDrive2 = new PWMSparkMax(1);
+    private final PWMSparkMax rightDrive1 = new PWMSparkMax(2);
+    private final PWMSparkMax rightDrive2 = new PWMSparkMax(3);
+    private final MotorControllerGroup LMotors = new MotorControllerGroup(leftDrive2, leftDrive1);
+    private final MotorControllerGroup RMotors = new MotorControllerGroup(rightDrive2, rightDrive1);
+    public final DifferentialDrive robotDrive = new DifferentialDrive(LMotors, RMotors);
+    XboxController m_driverController = new XboxController(0);
+
+    /**
+     * This function is run when the robot is first started up and should be used
+     * for any
+     * initialization code.
+     */
+    @Override
+    public void robotInit() {
+        // We need to invert one side of the drivetrain so that positive voltages
+        // result in both sides moving forward. Depending on how your robot's
+        // gearbox is constructed, you might have to invert the left side instead.
+        // RMotors.setInverted(true);
+    }
+
+    Forward auton;//DEPENDANT ON AUTON USED---------------------------------------------------███ 1 ███
+    /** This function is run once each time the robot enters autonomous mode. */
+    @Override
+    public void autonomousInit() {
+        auton=new Forward(0.5,1.2);//DEPENDANT ON AUTON USED---------------------------------███ 1 ███
+    }
+
+    /** This function is called periodically during autonomous. */
+    @Override
+    public void autonomousPeriodic() {
+        auton.loop(robotDrive);//PARAMATERS DEPENDANT ON AUTON USED--------------------------███ 1 ███
+    }
+
+    /**
+     * This function is called once each time the robot enters teleoperated mode.
+     */
+    @Override
+    public void teleopInit() {
+    }
+
+    /** This function is called periodically during teleoperated mode. */
+    @Override
+    public void teleopPeriodic() {
+        robotDrive.tankDrive(m_driverController.getLeftY(), m_driverController.getRightY());
+    }
+
+    /** This function is called once each time the robot enters test mode. */
+    @Override
+    public void testInit() {
+    }
+
+    /** This function is called periodically during test mode. */
+    @Override
+    public void testPeriodic() {
+    }
+}
