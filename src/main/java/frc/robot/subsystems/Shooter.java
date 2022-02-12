9d394889196f679421409c85fe470f0258f79e06
Austin Spencer <aspencer527@gmail.com>
2022-02-12 12:19:26 -0500

    removed unuesed imoprts

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -3,8 +3,6 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 
-import edu.wpi.first.wpilibj.XboxController;
-
 public class Shooter {
     public void teleloop(double speed, TalonFX falconMotor, boolean go) {
         if (go) {
