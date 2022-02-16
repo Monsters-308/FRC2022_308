8b82da3f8632eab68cb381e2f1bbabaf996d75d8
ValidName87 <ctlos1@gmail.com>
2022-02-15 20:29:20 -0500

    Added intake subsystem

---
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -0,0 +1,45 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.IntakeConstants;
+
+public class IntakeSubsystem extends SubsystemBase {
+    private final WPI_TalonSRX m_intakeMotor = new WPI_TalonSRX(IntakeConstants.kIntakeMotorPort);
+    private final WPI_TalonSRX m_retractMotor = new WPI_TalonSRX(IntakeConstants.kRetractMotorPort);
+
+    public IntakeSubsystem() {
+        m_intakeMotor.setNeutralMode(NeutralMode.Coast);
+        m_retractMotor.setNeutralMode(NeutralMode.Coast);
+    }
+
+    /**
+     * Forward Intake.
+     */
+    public void forwardIntake() {
+        m_intakeMotor.set(ControlMode.PercentOutput, IntakeConstants.kIntakeMotorForwardSpeed);
+    }
+
+    /**
+     * Reverse Intake.
+     */
+    public void reverseIntake() {
+        m_intakeMotor.set(ControlMode.PercentOutput, IntakeConstants.kIntakeMotorReverseSpeed);
+    }
+
+    /**
+     * Stop Intake.
+     */
+    public void stopIntake() {
+        m_intakeMotor.set(ControlMode.PercentOutput, 0);
+    }
+
+    @Override
+    public void periodic() {
+
+    }
+}
