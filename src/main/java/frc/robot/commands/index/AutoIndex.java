93469cc79ae03b2e5a31042328627b55e83f478b
ValidName87 <ctlos1@gmail.com>
2022-02-24 16:09:29 -0500

    Simplified AutoIndex

--- src/main/java/frc/robot/commands/index/AutoIndex.java
+++ src/main/java/frc/robot/commands/index/AutoIndex.java
@@ -5,16 +5,8 @@ import frc.robot.subsystems.IndexSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
 
 public class AutoIndex extends CommandBase {
-    public enum BallStage {
-        EMPTY,
-        BALL_PRESENT,
-        BALL_INDEXED,
-        FULL
-    }
-
     private final IndexSubsystem m_indexSubsystem;
     private final IntakeSubsystem m_intakeSubsystem;
-    public BallStage m_ballStage = BallStage.EMPTY;
     public boolean m_complete = false;
 
     public AutoIndex(IndexSubsystem indexSubsystem, IntakeSubsystem intakeSubsystem) {
@@ -26,57 +18,18 @@ public class AutoIndex extends CommandBase {
 
     @Override
     public void initialize() {
-        if (m_indexSubsystem.isUpperBallPresent() && m_indexSubsystem.isLowerBallPresent()) {
-            m_ballStage = BallStage.FULL;
-            m_indexSubsystem.stopIndex();
-            m_intakeSubsystem.stopIntake();
-        } else if (m_indexSubsystem.isUpperBallPresent() && !m_indexSubsystem.isLowerBallPresent()) {
-            m_ballStage = BallStage.BALL_INDEXED;
-        } else if (!m_indexSubsystem.isUpperBallPresent() && m_indexSubsystem.isLowerBallPresent()) {
-            m_ballStage = BallStage.BALL_PRESENT;
-        } else {
-            m_ballStage = BallStage.EMPTY;
-        }
-    }
-
-    @Override
-    public void execute() {
-        switch (m_ballStage) {
-            case EMPTY:
-                if (m_indexSubsystem.isLowerBallPresent()) {
-                    m_ballStage = BallStage.BALL_PRESENT;
-                }
-                m_intakeSubsystem.runIntake();
-                m_indexSubsystem.runIndex();
-                break;
-            case BALL_PRESENT:
-                if (m_indexSubsystem.isUpperBallPresent()) {
-                    m_ballStage = BallStage.BALL_INDEXED;
-                } else {
-                    m_indexSubsystem.runIndex();
-                    m_intakeSubsystem.runIntake();
-                }
-                break;
-            case BALL_INDEXED:
-                if (m_indexSubsystem.isLowerBallPresent() && m_indexSubsystem.isUpperBallPresent()) {
-                    m_ballStage = BallStage.FULL;
-                    m_indexSubsystem.stopIndex();
-                    m_intakeSubsystem.stopIntake();
-                } else {
-                    m_intakeSubsystem.runIntake();
-                }
-                break;
-            case FULL:
-                m_intakeSubsystem.stopIntake();
-                m_indexSubsystem.stopIndex();
-                m_complete = true;
-                break;
-
+        if (!(m_indexSubsystem.isUpperBallPresent() && m_indexSubsystem.isLowerBallPresent())) {
+            m_indexSubsystem.runIndex();
+            m_intakeSubsystem.runIntake();
         }
     }
 
     @Override
     public boolean isFinished() {
-        return m_complete;
+        if (m_indexSubsystem.isUpperBallPresent() && m_indexSubsystem.isLowerBallPresent()) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
