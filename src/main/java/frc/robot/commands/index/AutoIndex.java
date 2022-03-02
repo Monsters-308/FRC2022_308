329572a6579e9a5ca7fb1a756496ba7d69453e17
Austin Spencer <aspencer527@gmail.com>
2022-03-01 19:50:40 -0500

    removed unuesd import in autoindex

--- src/main/java/frc/robot/commands/index/AutoIndex.java
+++ src/main/java/frc/robot/commands/index/AutoIndex.java
@@ -1,7 +1,6 @@
 package frc.robot.commands.index;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.Constants.IntakeConstants;
 import frc.robot.subsystems.IndexSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
 
