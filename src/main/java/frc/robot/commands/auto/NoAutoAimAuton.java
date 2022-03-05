e68007517f0f3c3dfc3811ad488ab5f59bd097b5
Austin Spencer <aspencer527@gmail.com>
2022-03-05 11:08:02 -0500

    Added NoAimAuton

---
+++ src/main/java/frc/robot/commands/auto/NoAutoAimAuton.java
@@ -0,0 +1,28 @@
+package frc.robot.commands.auto;
+
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.drive.DriveDistance;
+import frc.robot.commands.drive.DriveTurn;
+import frc.robot.commands.index.AutoIndex;
+import frc.robot.commands.shooter.AutoShooter;
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.IndexSubsystem;
+import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.LEDSubsystem;
+import frc.robot.subsystems.ShooterSubsystem;
+
+public class NoAutoAimAuton extends SequentialCommandGroup {
+    public NoAutoAimAuton(DriveSubsystem drive, IntakeSubsystem intake, ShooterSubsystem shooter, IndexSubsystem index,
+            LEDSubsystem leads) {
+        addCommands(
+                new SequentialCommandGroup(
+                        new ParallelCommandGroup(
+                                new DriveDistance(35, 0.45, drive),
+                                new AutoIndex(index, intake, leads)),
+                        new DriveTurn(180, 0.4, 0.02, drive),
+                        new DriveDistance(25, 0.45, drive),
+                        new AutoShooter(index, shooter, leads)));
+
+    }
+}
