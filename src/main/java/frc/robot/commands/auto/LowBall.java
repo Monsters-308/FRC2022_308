71ca50373f402e737789ed5b2d1c99f0882b7874
ValidName87 <ctlos1@gmail.com>
2022-03-16 11:42:34 -0400

    Cleaned warnings

--- src/main/java/frc/robot/commands/auto/LowBall.java
+++ src/main/java/frc/robot/commands/auto/LowBall.java
@@ -1,10 +1,7 @@
 package frc.robot.commands.auto;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.commands.drive.DriveDistance;
 import frc.robot.commands.shooter.AutoLowShooter;
-import frc.robot.commands.shooter.AutoShooter;
-import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.IndexSubsystem;
 import frc.robot.subsystems.LEDSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
@@ -13,7 +10,6 @@ public class LowBall extends SequentialCommandGroup {
     public LowBall(ShooterSubsystem shooter, IndexSubsystem index, LEDSubsystem led) {
         addCommands(new SequentialCommandGroup(
 
-                new AutoLowShooter  (index, shooter, led)));
+                new AutoLowShooter(index, shooter, led)));
     }
 }
-
