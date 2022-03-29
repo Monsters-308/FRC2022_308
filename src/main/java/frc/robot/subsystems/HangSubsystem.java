0acf347dcef6fd66b09359678ccaec5c64e47d03
Austin Spencer <aspencer527@gmail.com>
2022-03-29 19:04:30 -0400

    Separated rotating arms into their own subsystem

--- src/main/java/frc/robot/subsystems/HangSubsystem.java
+++ src/main/java/frc/robot/subsystems/HangSubsystem.java
@@ -1,29 +1,20 @@
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.IdleMode;
 
 import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.PneumaticsControlModule;
-import edu.wpi.first.wpilibj.PneumaticsModuleType;
-import edu.wpi.first.wpilibj.Solenoid;
 
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants.PneumaticsConstants;
 
 import static frc.robot.Constants.HangConstants;
 
 public class HangSubsystem extends SubsystemBase {
     private final CANSparkMax m_stationaryMotorLeft = new CANSparkMax(HangConstants.kLeftNeoPort, MotorType.kBrushless);
     private final CANSparkMax m_stationaryMotorRight = new CANSparkMax(HangConstants.kRightNeoPort, MotorType.kBrushless);
-    private final WPI_TalonSRX m_rotatingMotorLeft = new WPI_TalonSRX(HangConstants.kLeft775Port);
-    private final WPI_TalonSRX m_rotatingMotorRight = new WPI_TalonSRX(HangConstants.kRight775Port);
-    private final Solenoid m_leftPiston = new Solenoid(PneumaticsConstants.kControlModulePort, PneumaticsModuleType.CTREPCM, PneumaticsConstants.kLeftHangPistonPort);
-    private final Solenoid m_rightPiston = new Solenoid(PneumaticsConstants.kControlModulePort, PneumaticsModuleType.CTREPCM, PneumaticsConstants.kRightHangPistonPort);
-
+    
     private final DigitalInput m_upperLeftSwitch = new DigitalInput(HangConstants.kUpperLeftSwitchPort);
     private final DigitalInput m_lowerLeftSwitch = new DigitalInput(HangConstants.kLowerLeftSwitchPort);
     private final DigitalInput m_upperRightSwitch = new DigitalInput(HangConstants.kUpperRightSwitchPort);
---
+++ src/main/java/frc/robot/subsystems/RotatingHangSubsystem.java
@@ -0,0 +1,58 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.HangConstants;
+import frc.robot.Constants.PneumaticsConstants;
+
+public class RotatingHangSubsystem extends SubsystemBase {
+    private final WPI_TalonSRX m_rotatingMotorLeft = new WPI_TalonSRX(HangConstants.kLeft775Port);
+    private final WPI_TalonSRX m_rotatingMotorRight = new WPI_TalonSRX(HangConstants.kRight775Port);
+    private final Solenoid m_leftPiston = new Solenoid(PneumaticsConstants.kControlModulePort,
+            PneumaticsModuleType.CTREPCM, PneumaticsConstants.kLeftHangPistonPort);
+    private final Solenoid m_rightPiston = new Solenoid(PneumaticsConstants.kControlModulePort,
+            PneumaticsModuleType.CTREPCM, PneumaticsConstants.kRightHangPistonPort);
+
+    public RotatingHangSubsystem() {
+        m_rotatingMotorLeft.setNeutralMode(NeutralMode.Brake);
+        m_rotatingMotorRight.setNeutralMode(NeutralMode.Brake);
+        m_rotatingMotorLeft.setInverted(true);
+    }
+
+    public void runLeftHang() {
+        m_rotatingMotorLeft.set(TalonSRXControlMode.PercentOutput, HangConstants.kHangMotorSpeed);
+    }
+
+    public void stopLeftHang() {
+        m_rotatingMotorLeft.set(TalonSRXControlMode.PercentOutput, 0);
+    }
+
+    public void reverseLeftHang() {
+        m_rotatingMotorLeft.set(TalonSRXControlMode.PercentOutput, -HangConstants.kHangMotorSpeed);
+    }
+
+    public void runRightHang() {
+        m_rotatingMotorRight.set(TalonSRXControlMode.PercentOutput, HangConstants.kHangMotorSpeed);
+    }
+
+    public void stopRightHang() {
+        m_rotatingMotorRight.set(TalonSRXControlMode.PercentOutput, 0);
+    }
+
+    public void reverseRightHang() {
+        m_rotatingMotorRight.set(TalonSRXControlMode.PercentOutput, -HangConstants.kHangMotorSpeed);
+    }
+
+    public void setLeftDeployed(boolean forward) {
+        m_leftPiston.set(forward);
+    }
+
+    public void setRightDeployed(boolean forward) {
+        m_rightPiston.set(forward);
+    }
+}
